00b08f245d7328d82acc514d5c2d530eb2d5aaf1
thilina-fer <dilshanthilina1010@gmail.com>
2025-10-22 05:06:33 +0530

    update

---
+++ script.js
@@ -0,0 +1,19 @@
+const navLinks = document.querySelectorAll(".nav-link");
+const sections = document.querySelectorAll("section, header");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 75) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href").includes(current)) {
+      link.classList.add("active");
+    }
+  });
+});
